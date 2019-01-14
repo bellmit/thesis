2f845279db421d0f83b2ef5cd80745616420f27a
Henrik Smith <henriksmith@hotmail.dk>
2019-01-14 11:46:12 +0100

    changed filepath to platform instead of com.nikolaj.thesis.thesis

---
+++ src/main/java/platform/ThesisApplication.java
@@ -0,0 +1,36 @@
+package platform;
+
+
+
+import platform.storage.StorageProperties;
+import platform.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class ThesisApplication  {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+//			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
+
